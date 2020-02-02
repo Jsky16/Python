ebf9f6850a5469fe23b7eda32049f6799e444391
Subham <43762603+Jsky16@users.noreply.github.com>
2020-02-02 20:55:27 +0530

    Delete Unpickling.py

--- Unpickling.py
+++
@@ -1,9 +0,0 @@
-import pickle,Employee
-with open('emp.dat','rb') as f:
-    while True:
-        try:
-            obj=pickle.load(f)
-            obj.Display()
-        except EOFError:
-            print('Unpickling of object is completed')
-            break
